873a3793a29b0d899d74d2e9f308916429d10e0c
pratyush2905 <pratyushdubey2021@gmail.com>
2024-03-04 22:20:58 +0530

    A_Pashmak_and_Garden

--- A_Pashmak_and_Garden.cpp
+++ A_Pashmak_and_Garden.cpp
@@ -1,13 +1,31 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+#define int long long
+#define fr(a, b) for (int i = a; i < b; i++)
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define all(x) (x).begin(), (x).end()
+#define mod 1000000007
 using namespace std;
-int main()
+void solve()
 {
-    int x1,y1,x2,y2,x3,y3,x4,y4;
-    cin>>x1>>y1>>x2>>y2;
-    int a,d;
-    if(x1==x2 || y1==y2)
-    {
-        int d = abs(abs(x1)-abs(x2))+abs(abs(y1)-abs(y2));
-    }
 
+    int x1, y1, x2, y2;
+    cin >> x1 >> y1 >> x2 >> y2;
+    if (x1 != x2 && y1 != y2 && abs(x1 - x2) != abs(y1 - y2))
+        cout << -1 << endl;
+    else if (x1 == x2)
+        cout << x1 + abs(y1 - y2) << " " << y1 << " " << x2 + abs(y1 - y2) << " " << y2 << endl;
+    else if (y1 == y2)
+        cout << x1 << " " << y1 + abs(x1 - x2) << " " << x2 << " " << y2 + abs(x1 - x2) << endl;
+    else
+        cout << x1 << " " << y2 << " " << x2 << " " << y1 << endl;
+}
+signed main()
+{
+    int t;
+    t=1;
+    //cin >> t;
+    while (t--)
+    {
+        solve();
+    }
 }
